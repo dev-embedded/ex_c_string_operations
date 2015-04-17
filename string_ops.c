6f93b78f82c4a88be00033300812ae1f437ea5a0
dev-embedded <suntaoas@hotmail.com>
2015-04-17 09:05:06 -0400

    TaoSun: create the fonction buff_create().

--- string_ops.c
+++ string_ops.c
@@ -1,62 +1,65 @@
 #include "string_ops.h"
 
 /**
-* create a buffer, size is #define SIZE_OF_BUFFER
+* 1. create a buffer, size is #define SIZE_OF_BUFFER
 */
-char* buff_create();
+char* buff_create()
+{
+	return ((char*)(malloc(sizeof(char) * SIZE_OF_BUFFER)));
+}
 
 /**
-* free a buffer
+* 2. free a buffer
 */
 BOOL buff_free(char *buff);
 
 /**
-* init the 1st char of buff to "\0"
+* 3. init the 1st char of buff to "\0"
 */
 BOOL buff_clean(char *buff);
 
 /**
-* return the size of string, char* str is the original string.
+* 4. return the size of string, char* str is the original string.
 */
 int str_getlen(char* str);
 
 /**
-* return the status of buffer (full)
+* 5. return the status of buffer (full)
 */
 BOOL buff_isfull(char* str);
 
 /**
-* return the status of buffer (empty)
+* 6. return the status of buffer (empty)
 */
 BOOL buff_isempty(char* str);
 
 /**
-* return the first location of char or the first char of substring
+* 7. return the first location of char or the first char of substring
 */
 int str_getlocation(char* str, char* substr);
 
 /**
-* insert a char or a substring to the string
+* 8. insert a char or a substring to the string
 */
 char* str_insert(char* str, char* substr, int index);
 
 /**
-* delete all of the char or the substring from the string
+* 9. delete all of the char or the substring from the string
 */
 char* str_del(char* str, char* substr);
 
 /**
-* replace all of the char or the substring with another in the string
+* 10. replace all of the char or the substring with another in the string
 */
 char* str_replace(char* str, char* rep, char* origin);
 
 /**
-* sort of string(from small to big with ASCII code)
+* 11. sort of string(from small to big with ASCII code)
 */
 char* str_sort(char* str);
 
 /**
-* return the times of a substring repeat in a string
+* 12. return the times of a substring repeat in a string
 */
 int str_rep(char* str, char* substr);
 
