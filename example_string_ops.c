95e02c94aeb75358bf10d285aeabf18d687a9f17
dev-embedded <suntaoas@hotmail.com>
2015-04-19 12:21:28 -0400

    TaoSun: create the function NO.10, but not finished

--- example_string_ops.c
+++ example_string_ops.c
@@ -4,12 +4,19 @@
 
 int main()
 {
-	char* str = "abcedefasidefdfz";
+	char* str = buff_create();
+	str = "abcedefa";
+	printf("str:%s\n",str);
 	char* substr = "def";
+	char* rep = "zaqz";
 	int len_str = str_getlen(str);
-	printf("str_getlen() : %d\n",len_str);
-	printf("str_getlocation(def) : %d\n",str_getlocation(str,substr));
-	printf("hello , world !\n");
+	printf("=====test of operation of the string======\n");
+	printf("4.str_getlen() : %d\n",len_str);
+	printf("--------------------\n");
+	printf("7.str_getlocation(def) : %d\n",str_getlocation(str,substr));
+	printf("--------------------\n");
+	printf("10.str_replace(str, rep, substr) : %s\n",str_replace(str, rep, substr));
+	printf("--------------------\n");
 	return 0;
 }
 
--- string_ops.c
+++ string_ops.c
@@ -1,64 +1,58 @@
+#include <string.h>
 #include "string_ops.h"
 
 /**
-* 1. create a buffer, size is #define SIZE_OF_BUFFER
-*/
-char* buff_create()
-{
-	return ((char*)(malloc(sizeof(char) * SIZE_OF_BUFFER)));
+ * 1. create a buffer, size is #define SIZE_OF_BUFFER
+ */
+char* buff_create() {
+	return ((char*) (malloc(sizeof(char) * SIZE_OF_BUFFER)));
 }
 
 /**
-* 2. free a buffer
-*/
+ * 2. free a buffer
+ */
 BOOL buff_free(char *buff);
 
 /**
-* 3. init the 1st char of buff to "\0"
-*/
+ * 3. init the 1st char of buff to "\0"
+ */
 BOOL buff_clean(char *buff);
 
 /**
-* 4. return the size of string, char* str is the original string.
-*/
-int str_getlen(char* str)
-{
+ * 4. return the size of string, char* str is the original string.
+ */
+int str_getlen(char* str) {
 	int len = 0;
-	while('\0' != *(str + len))
-	{
+	while ('\0' != *(str + len)) {
 		len++;
 	}
 	return len;
 }
 
 /**
-* 5. return the status of buffer (full)
-*/
+ * 5. return the status of buffer (full)
+ */
 BOOL buff_isfull(char* str);
 
 /**
-* 6. return the status of buffer (empty)
-*/
+ * 6. return the status of buffer (empty)
+ */
 BOOL buff_isempty(char* str);
 
 /**
-* 7. return the first location of char or the first char of substring
-*/
-int str_getlocation(char* str, char* substr)
-{
+ * 7. return the first location of char or the first char of substring
+ * 	if return -1 , it means that no find.
+ */
+int str_getlocation(char* str, char* substr) {
 	int len_str = 0;
 	int len_substr = str_getlen(substr);
 	BOOL res = FALSE;
-	while(('\0' != *(str + len_str)) && !res)
-	{
-		if(*(substr) == *(str +len_str))
-		{
+	while (('\0' != *(str + len_str)) && !res) {
+		if (*(substr) == *(str + len_str)) {
 			res = TRUE;
 			int i;
-			for(i=0;i<len_substr;i++)
-			{
-				if(*(str + len_str + i) != *(substr + i))
-				{
+			for (i = 0; i < len_substr; i++) {
+				if (*(str + len_str + i) != *(substr + i)) {
 					res = FALSE;
 					break;
 				}
@@ -66,59 +60,81 @@ int str_getlocation(char* str, char* substr)
 		}
 		len_str++;
 	}
-	return (len_str - 1);
-}
-
-/**
-* 8. insert a char or a substring to the string
-*/
-char* str_insert(char* str, char* substr, int index);
-
-/**
-* 9. delete all of the char or the substring from the string
-*/
-char* str_del(char* str, char* substr);
-
-/**
-* 10. replace all of the char or the substring with another in the string
-*/
-char* str_replace(char* str, char* rep, char* origin)
-{
-	if(!str_compare(rep, origin))
-	{
-		int loc = 0;
-		while(loc < str_getlen(str))
-		{
-			loc = str_getlocation(str, origin);
-		}
+	if (res) {
+		return (len_str - 1);
+	} else {
+		return - 1;
 	}
 }
 
 /**
-* 11. sort of string(from small to big with ASCII code)
-*/
+ * 8. insert a char or a substring to the string
+ */
+char* str_insert(char* str, char* substr, int index);
+
+/**
+ * 9. delete all of the char or the substring from the string
+ */
+char* str_del(char* str, char* substr);
+
+/**
+ * 10. replace all of the char or the substring with another in the string
+ */
+char* str_replace(char* str, char* rep, char* origin) {
+	if (!str_compare(rep, origin)) {
+		int loc = 0;
+		char* str_temp = (char*) (malloc(sizeof(char) * 100));
+		*(str_temp) = '\0';
+		char* p = str;
+		int i = 0, j = 0;
+		int str_temp_len;
+		int origin_len = str_getlen(origin);
+		int rep_len = str_getlen(rep);
+		while (-1 != (loc = str_getlocation(p, origin))) {
+			loc = str_getlocation(p, origin);
+			str_temp_len = str_getlen(str_temp);
+			for (i = 0; i < loc; i++) {
+				*(str_temp + str_temp_len + i) = *(p + i);
+			}
+			for (j = 0; j < rep_len; j++) {
+				*(str_temp + str_temp_len + i + j) = *(rep + j);
+			}
+			*(str_temp + str_temp_len + i + j + 1) = '\0';
+			p = p + loc + origin_len;
+		}
+		//for (j = 0; j < str_getlen(str_temp); j++) {
+		//	*(str + j) = *(str_temp + j);
+		//	*(str + j + 1) = '\0';
+		//}
+		//*(str) = '\0';
+		//strcpy(str, str_temp);
+		printf("str_temp:%s\n",str_temp);
+		//printf("str:%s\n",str);
+	}
+	return str;
+}
+
+/**
+ * 11. sort of string(from small to big with ASCII code)
+ */
 char* str_sort(char* str);
 
 /**
-* 12. return the times of a substring repeat in a string
-*/
+ * 12. return the times of a substring repeat in a string
+ */
 int str_rep(char* str, char* substr);
 
 /**
-* 13. compare between two string
-*/
-BOOL str_compare(char* str, char* substr)
-{
+ * 13. compare between two string
+ */
+BOOL str_compare(char* str, char* substr) {
 	BOOL res = TRUE;
-	if(str_getlen(str) != str_getlen(substr))
-	{
+	if (str_getlen(str) != str_getlen(substr)) {
 		res = FALSE;
-	}else{
+	} else {
 		int i = 0;
-		while('\0' != *(str + i) && '\0' != *(substr + i))
-		{
-			if(*(str + i) != *(substr + i))
-			{
+		while ('\0' != *(str + i) && '\0' != *(substr + i)) {
+			if (*(str + i) != *(substr + i)) {
 				res = FALSE;
 				break;
 			}
@@ -128,8 +144,7 @@ BOOL str_compare(char* str, char* substr)
 }
 
 /**
-* 14. copy a string from buffer
-*/
+ * 14. copy a string from buffer
+ */
 char* str_copy(char* str);
-
 
